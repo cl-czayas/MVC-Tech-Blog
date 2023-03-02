23f240d0b5f493cb1316489a0bb23126e5841032
CL <cl.czayas@gmail.com>
2023-03-02 16:04:09 -0400

    Added models & schema

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+    {
+    // Manually define the primary key
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+    },
+    text: {
+        type: DataTypes.STRING,
+    }
+    },
+    {
+    sequelize,
+    timestamps: false,
+    // Prevent sequelize from renaming the table
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+    }
+);
+
+module.exports = Post;
